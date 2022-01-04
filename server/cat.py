4ea44f0e2555f7215c45dacb0c97ba391907fa7a
Szymon Słowicki <slowicki7@wp.pl>
2022-01-05 00:30:36 +0100

    Add a new hash options

--- server/cat.py
+++ server/cat.py
@@ -6,7 +6,7 @@ from functools import wraps
 from flask_socketio import SocketIO, emit
 from flask_bcrypt import Bcrypt
 from flask_hashing import Hashing
-
+import hashlib
 app = Flask(__name__)
 bcrypt = Bcrypt()
 hashing = Hashing(app)
@@ -35,51 +35,3 @@ def welcome():
     return render_template('welcome.html')
 
 @app.route('/how')
-def how():
-    return render_template('how.html')
-
-@app.route('/about')
-def about():
-    return render_template('aboutme.html')
-
-@app.route('/apka', methods=['GET', 'POST'])
-def apka():
-    afterHash = None
-    if request.method == 'POST':
-        if  request.form['haslo'] == '':
-            flash('Empty textbox, try again!')
-        else:
-            afterHash = request.form['haslo']
-            #flash(bcrypt.generate_password_hash(afterHash))
-            flash(request.form['type'])
-
-    return render_template('apka.html')
-@app.route('/signal', methods=['POST'])
-def signal():
-    socketio.emit(request.json['sig'], [], broadcast=True)
-    #emit(request.json['sig'])
-    return request.json['sig']
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('You were just log in!')
-            return redirect(url_for('welcome'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    session.pop('logged_in', None)
-    flash('You were just log out!')
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
